9df856eb7dd9265a0767a2e5fc7009f89234430f
HebiJS <niklasheine2003@gmail.com>
2025-09-01 18:15:20 +0200

    Add button component

---
+++ src/routes/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export const { color, title } = $props()
+</script>
+
+{#if color === "secondary"}
+<button class="rounded-lg p-2 w-4/5 max-w-72 bg-gray-700 text-2xl tracking-tight active:bg-gray-800 focus:outline-2 focus:outline-gray-500">{title}</button>
+{/if}
+
+{#if color === "accent"}
+<button class="rounded-lg p-2 w-4/5 max-w-72 bg-green-900 text-2xl tracking-tight active:bg-green-800 focus:outline-2 focus:outline-green-500">{title}</button>
+{/if}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,6 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import Button from "../lib/components/Button.svelte";
+</script>
 
 <svelte:head>
     <title>Aids!</title>
@@ -7,8 +9,8 @@
 <main class="flex flex-col gap-2 justify-center items-center h-svh bg-gray-900 text-gray-100">
     <h1 class="mb-8 text-5xl text-bold tracking-wider">Account</h1>
 
-    <button class="rounded-lg p-2 w-4/5 max-w-72 bg-gray-700 text-2xl tracking-tight active:bg-gray-800 focus:outline-2 focus:outline-gray-500">Login</button>
-    <button class="rounded-lg p-2 w-4/5 max-w-72 bg-gray-700 text-2xl tracking-tight active:bg-gray-800 focus:outline-2 focus:outline-gray-500">Register</button>
+    <Button title="Login" color="secondary" />
+    <Button title="Register" color="accent" />
 </main>
 
 <style lang="postcss">
