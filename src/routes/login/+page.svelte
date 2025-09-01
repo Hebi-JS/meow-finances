8064a66f16181476ec5f2d304313faa0ad6510f5
HebiJS <niklasheine2003@gmail.com>
2025-09-01 18:07:15 +0200

    Add babushka favicon and login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts"></script>
+
+<svelte:head>
+    <title>Aids!</title>
+</svelte:head>
+
+<main class="flex flex-col gap-2 justify-center items-center h-svh bg-gray-900 text-gray-100">
+    <h1 class="mb-8 text-5xl text-bold tracking-wider">Account</h1>
+
+    <button class="rounded-lg p-2 w-4/5 max-w-72 bg-gray-700 text-2xl tracking-tight active:bg-gray-800 focus:outline-2 focus:outline-gray-500">Login</button>
+    <button class="rounded-lg p-2 w-4/5 max-w-72 bg-gray-700 text-2xl tracking-tight active:bg-gray-800 focus:outline-2 focus:outline-gray-500">Register</button>
+</main>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+</style>
